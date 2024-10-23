2c1a451b57353006b820829d2b53513fd387ec17
hajirah84 <hajirahtdotmian@gmail.com>
2024-10-23 13:35:24 -0400

    Add files via upload

---
+++ flatten.js
@@ -0,0 +1,27 @@
+const eqArrays = function (arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false; 
+        }
+    }
+    return true; // 
+}
+
+const assertArrayEqual = function (arr1, arr2) {
+    if (eqArrays(arr1, arr2)) {
+        console.log ("✅✅✅")
+    } else {
+        console.log("🛑🛑🛑")
+    }
+}
+
+const flatten = function(arr){
+    if (arr.isArray) {
+        console.log(arr.flat());
+    }
+}
