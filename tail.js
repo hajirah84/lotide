dcf1804b8e193beb6b85ee888f63bec8a4656b7f
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:08:00 -0500

    tail test

--- tail.js
+++ tail.js
@@ -1,6 +1,7 @@
 const tail = function (arr) {
     return arr.slice(1)
 }
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(words.length === 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// console.log(words.length === 3); // original array should still have 3 elements!
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,4 @@
+const assertEqual = require("../assertequal")
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+console.log(words.length === 3); // original array should still have 3 elements!
