897106cc2d8dc4a1bb66d5ce385d53ed7ac01319
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:23:25 -0500

    tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
 const assertEqual = require("../assertequal")
 const tail = require ("../tail")
 assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+// Tests
 const words = ["Hello", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);  
+
+const singleElementArray = ["Hello"];
+const singleResult = tail(singleElementArray);
+assertEqual(singleResult.length, 0); 
