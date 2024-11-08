c9034061a5acf711f00d6b08ed05b8f418799de6
hajirah84 <mianhajirah@gmail.com>
2024-11-08 15:08:41 -0500

    middletest

--- middle.js
+++ middle.js
@@ -1,23 +1,23 @@
-const eqArrays = function (arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
+// const eqArrays = function (arr1, arr2) {
+//     if (arr1.length !== arr2.length) {
+//         return false;
+//     }
 
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return false; 
-        }
-    }
-    return true; 
-};
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i] !== arr2[i]) {
+//             return false; 
+//         }
+//     }
+//     return true; 
+// };
 
-const assertArraysEqual = function (arr1, arr2) {
-    if (eqArrays(arr1, arr2)) {
-        console.log(`✅✅✅ Assertion Passed: ${inspect(arr1)} === ${inspect(arr2)}`);
-    } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${inspect(arr1)} !== ${inspect(arr2)}`);
-    }
-};
+// const assertArraysEqual = function (arr1, arr2) {
+//     if (eqArrays(arr1, arr2)) {
+//         console.log(`✅✅✅ Assertion Passed: ${inspect(arr1)} === ${inspect(arr2)}`);
+//     } else {
+//         console.log(`🛑🛑🛑 Assertion Failed: ${inspect(arr1)} !== ${inspect(arr2)}`);
+//     }
+// };
 
 const middle = function(arr) {
     const length = arr.length; 
@@ -32,6 +32,6 @@ const middle = function(arr) {
         return [arr[i]]; 
     }
 };
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle; 
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
