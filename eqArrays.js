225a42c76b17a66a69ff27f9bf6284fd649233cd
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:37:34 -0500

    edits

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,9 @@
-const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
+// const assertEqual = function (actual, expected) {
+//     if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//     } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//     }
 const eqArrays = function (arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false;
