8ee124d86fd2ed999261474d4ea2dd54870e70f2
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:55:42 -0500

    edits

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,2 +1,4 @@
-const assertArrasEqual = require ("../assertArraysEqual")
+const eqArrays = require ("../eqArrays");
+const assertArraysEqual = require ("../assertArraysEqual")
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual ([1,4,5,6], [9,0,3,4,2,6,0,5])
