db8e0c31bcef062521d3383fcc8a21246dc1a4bd
hajirah84 <mianhajirah@gmail.com>
2024-11-08 15:19:30 -0500

    edit

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,5 @@
 const assertArraysEqual = require ("../assertArraysEqual")
+const eqArrays = require ("../eqArrays");
 const middle = require ("../middle")
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
