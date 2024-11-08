3722da8c38b67ee56f7758db39c574a1293c2193
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:18:24 -0500

    tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,5 @@
 const assertEqual = require("../assertequal")
 const tail = require ("../tail")
 assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs"); 
