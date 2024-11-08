fd47720979c117a49fe0f701ab253aeb42da8f97
hajirah84 <mianhajirah@gmail.com>
2024-11-08 14:13:10 -0500

    tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,3 @@
 const assertEqual = require("../assertequal")
 const tail = require ("../tail")
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log(words.length === 3); // original array should still have 3 elements!
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]);
