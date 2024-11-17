e7409ef907cdaff9e83a938f9514ccd84ccdc348
hajirah84 <mianhajirah@gmail.com>
2024-11-16 21:08:37 -0500

    fixed middle function issue

--- middle.js
+++ middle.js
@@ -39,5 +39,3 @@ module.exports = {
     assertArraysEqual: assertArraysEqual,
   };
 
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,6 @@
-// const { assertArraysEqual, middle } = require("../middle");
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
 const assert = require('chai').assert;
-const middle = require('../middle'); 
+const {middle} = require('../middle'); 
 
 describe("#middle", () => {
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
